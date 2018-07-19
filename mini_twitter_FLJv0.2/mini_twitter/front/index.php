bb4e6b8ded3499d29a3989a5e117d8580934a4d2
Lucas Lazarini <37162691+LucasLazarini@users.noreply.github.com>
2018-07-19 16:12:53 -0300

    Add files via upload

---
+++ mini_twitter_FLJv0.2/mini_twitter/front/index.php
@@ -0,0 +1,15 @@
+<html lang="pt-BR" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos_CSS/estiloIndex.css">
+    <title></title>
+  </head>
+  <body id="fundo">
+      <?php require_once("header.php")?>
+      <section>
+        <a href="registro.php" class="fill-div1"><b> Cadastre-se </b></a>
+        <a href="login.php" class="fill-div2"><b> Entrar </b></a>
+      </section>
+  </body>
+</html>
