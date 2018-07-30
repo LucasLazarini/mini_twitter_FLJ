b75c33950d313237b22d11414d6cb158fad15200
Lucas Lazarini <37162691+LucasLazarini@users.noreply.github.com>
2018-07-30 20:41:47 -0300

    Add files via upload

---
+++ mini_twitter_FLJ-v1.2/mini_twitter/js/alterarDados.js
@@ -0,0 +1,14 @@
+function enviar_formAlteraNome(){
+  document.formularioAlteraNome.submit();
+}
+function enviar_formAlteraUsername(){
+  document.formularioAlteraUsername.submit();
+}
+function enviar_formAlterarSenha(){
+  document.formularioAlterarSenha.submit();
+}
+function verificaVariavel(variavel){
+  if(empty(variavel)){
+
+  }
+}
