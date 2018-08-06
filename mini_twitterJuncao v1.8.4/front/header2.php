6ab96ef0c03ff8a247d11813452dd4df117b00e3
Lucas Lazarini <37162691+LucasLazarini@users.noreply.github.com>
2018-08-06 15:51:57 -0300

    Add files via upload

---
+++ mini_twitterJuncao v1.8.4/front/header2.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../estilos_CSS/estiloHeader2.css">
+<header id="header">
+  <a href="index.php"><img src="../imagens/logoLivro.png" id="logo"></a>
+  <a href="registro.php" id="botaoCadastrar"><b>Inscrever-se</b></a>
+  <a href="login.php" id="botaoLogin"><b>Entrar</b></a>
+</header>
